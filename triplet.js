0557a46719b8c29838108b5e56b94e887e5b48a4
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-31 15:21:37 +0530

    Data structres commiting:

---
+++ triplet.js
@@ -0,0 +1,19 @@
+function findTriplets(arr) {
+    let found = false;
+    for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+        for (let k = j + 1; k < arr.length; k++) {
+            if (arr[i] + arr[j] + arr[k] === 0)
+            {
+                console.log(arr[i]+" "+arr[j]+" "+arr[k]);
+                found = true;
+            }
+        }
+    }
+    if(found === false) {
+        console.log(" not exist ");
+    }
+}
+}
+arr = [0, -1, 2, -3, 1];
+findTriplets(arr); 
